98cf2be7affa5c255176301885c3468a9fab6253
Francesco Saccone <francescosaccone95@live.it>
2020-10-08 11:13:41 +0200

    Navbar implemented

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className="nav-box border-bottom">
+      <a className="logo" href="http://thoughtexperiences.com/"><img src="Logo_no_background.svg" alt="TE logo" /></a>
+      <span className="span-style">Events</span>
+        </div>
+
+  )
+}
+
+export default NavBar
