7139fc7f78d865bc300d99693af32d2cd1aba0c0
Francesco Saccone <francescosaccone95@live.it>
2020-10-20 18:38:13 +0200

    First two components implemented

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import NavBar from './NavBar'
-import Introduction from './Introduction'
-import Banner from './Banner'
 import './Home.css'
+import EssencePractical from './EssencePractical'
+import Paragraphs from './Paragraphs'
 
 function Home () {
-  const deepFocus = <h2 className="banner-general-inside">Deep <span className="bold-test">Focus</span></h2>
-  const designingFuture = <h2 className="banner-general-inside">Designing Your <span className="bold-test">Future</span></h2>
-  const structureRythmBalance = <h2 className="banner-general-inside"><span className="bold-test">Structure</span> | Rythm | Balance</h2>
+  
   return(
     <div>
         <NavBar />
-        <Introduction />
-        <Banner caption={deepFocus} />
-        <Banner caption={designingFuture} />
-        <Banner caption={structureRythmBalance} />
+        <EssencePractical />
+        <Paragraphs />
     </div>
   )
 }
