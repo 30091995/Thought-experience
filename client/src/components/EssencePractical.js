8227a877f538630842d4672804ca0e883d1c84a3
Francesco Saccone <francescosaccone95@live.it>
2020-11-02 15:24:59 +0100

    Github mistake

--- client/src/components/EssencePractical.js
+++ client/src/components/EssencePractical.js
@@ -3,29 +3,38 @@ import './EssencePractical.css'
 import { Container, Row, Col } from 'react-bootstrap'
 
 
-function EssencePractical ()
-{
-  return(
+function EssencePractical() {
+  return (
     <div>
-          
-          <div className="caption-container">
-          <h5 className="caption-style">HEADPHONES WORK BETTER :) </h5>
-          </div>
+
+      <div className="caption-container">
+        <h5 className="caption-style">HEADPHONES WORK BETTER :) </h5>
+      </div>
 
 
-          <div className="cont">
-          <div>    
-          <h3 className="essence-style">ESSENCE AND PRACTICAL APPLICATION</h3>
-          </div>
-          
-          <iframe height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/892013566&color=%23b6b6b6&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style={{fontSize: "10px", color: "#cccccc", lineBreak: "anywhere", wordBreak: "normal", overflow: "hidden" , whiteSpace: "nowrap" , textOverflow: "ellipsis", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif", fontWeight: "100"}}><a href="https://soundcloud.com/thoughtexperiences" title="Thought Experiences" target="_blank" style={{color: "#cccccc", textDecoration: "none"}}>Thought Experiences</a> · <a href="https://soundcloud.com/thoughtexperiences/what-are-thought-experiences-i" title="What Are Thought Experiences - I" target="_blank" style={{color: "#cccccc", textDecoration: "none"}}>What Are Thought Experiences - I</a></div>
 
-          
-          <iframe height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/892017181&color=%23b6b6b6&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style={{fontSize: "10px", color: "#cccccc", lineBreak: "anywhere", wordBreak: "normal", overflow: "hidden" , whiteSpace: "nowrap", textOverflow: "ellipsis", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif", fontWeight: "100"}}><a href="https://soundcloud.com/thoughtexperiences" title="Thought Experiences" target="_blank" style={{color: "#cccccc", textDecoration: "none"}}>Thought Experiences</a> · <a href="https://soundcloud.com/thoughtexperiences/what-are-thought-experiences-ii" title="What Are Thought Experiences - II" target="_blank" style={{color: "#cccccc", textDecoration: "none"}}>What Are Thought Experiences - II</a></div>
-          
-          </div>
 
-            
+      <Container>
+        <Row>
+          <Col lg="12">
+            <div>
+              <h3 className="essence-style">ESSENCE AND PRACTICAL APPLICATION</h3>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="12">
+            <iframe height="166" width="100%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/892013566&color=%23b6b6b6&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style={{ fontSize: "10px", color: "#cccccc", lineBreak: "anywhere", wordBreak: "normal", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif", fontWeight: "100" }}><a href="https://soundcloud.com/thoughtexperiences" title="Thought Experiences" target="_blank" style={{ color: "#cccccc", textDecoration: "none" }}>Thought Experiences</a> · <a href="https://soundcloud.com/thoughtexperiences/what-are-thought-experiences-i" title="What Are Thought Experiences - I" target="_blank" style={{ color: "#cccccc", textDecoration: "none" }}>What Are Thought Experiences - I</a></div>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="12">
+            <iframe height="166" width="100%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/892017181&color=%23b6b6b6&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style={{ fontSize: "10px", color: "#cccccc", lineBreak: "anywhere", wordBreak: "normal", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif", fontWeight: "100" }}><a href="https://soundcloud.com/thoughtexperiences" title="Thought Experiences" target="_blank" style={{ color: "#cccccc", textDecoration: "none" }}>Thought Experiences</a> · <a href="https://soundcloud.com/thoughtexperiences/what-are-thought-experiences-ii" title="What Are Thought Experiences - II" target="_blank" style={{ color: "#cccccc", textDecoration: "none" }}>What Are Thought Experiences - II</a></div>
+
+          </Col>
+        </Row>
+      </Container>
+
     </div>
   )
 }
